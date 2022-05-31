97d6ee529bf67d66c2c87c85f3b773e06bb7bf42
Slava <svstason@gmail.com>
2022-06-01 01:00:19 +0300

    Finish about. Need reorganize tooltip.

---
+++ src/js/components/tooltip.js
@@ -0,0 +1,8 @@
+import { createPopper} from '@popperjs/core';
+
+const el = document.querySelector('.tooltip__btn');
+const tooltip = document.querySelector('.tooltip__txt')
+
+createPopper(el, tooltip, {
+  placement: 'right-start'
+});
